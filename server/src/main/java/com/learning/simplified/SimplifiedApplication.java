29c5b3079f0c1103f6a77e9089fe7a5bf14b8796
Andres Segura <andres.segura.dev@gmail.com>
2023-10-09 20:03:15 -0500

    add: backend starter

---
+++ server/src/main/java/com/learning/simplified/SimplifiedApplication.java
@@ -0,0 +1,13 @@
+package com.learning.simplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedApplication.class, args);
+	}
+
+}
