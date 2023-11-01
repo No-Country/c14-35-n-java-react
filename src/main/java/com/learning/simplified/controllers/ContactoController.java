a8c36cdb80da6aed14baccbf187969e6dac9d814
“Laura <laurazeier@gmail.com>
2023-10-31 21:41:40 -0300

    Commit sobre el mail de LS. Se agregó contraseña generada desde GMAIL

--- src/main/java/com/learning/simplified/controllers/ContactoController.java
+++ src/main/java/com/learning/simplified/controllers/ContactoController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.learning.simplified.controllers;
 
 import com.learning.simplified.entities.Contacto;
@@ -12,10 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("/")
 public class ContactoController {
 
     @Autowired
@@ -25,25 +25,11 @@ public class ContactoController {
 
     @PostMapping("/contact")
     public ResponseEntity<Contacto> enviarMensaje(@RequestBody Contacto contacto) {
-        String from = "simplifiedlearning46@gmail.com";
+        
+        String from= "learningsimplified006@gmail.com"; 
+        
         contactoService.crearMensaje(from, contacto.getName(), contacto.getEmail(), contacto.getMessage(), contacto.getPhoneNumber());
 
         return ResponseEntity.ok().body(contacto);
     }
-
-    /*
-    @PostMapping("/contact")
-    public ResponseEntity<Contacto> enviarMensaje(@RequestBody String name,String email, String message,Long phoneNumber){
-        Contacto contacto = new Contacto(); 
-        String from="simplifiedlearning46@gmail.com"; 
-        contactoService.crearMensaje( from, name, email, message, phoneNumber);
-       
-        contacto.setEmail(email);
-        contacto.setMessage(message);
-        contacto.setPhoneNumber(phoneNumber);
-        contacto.setName(name);
-             
-        return ResponseEntity.ok().body(contacto);
-        
-    }*/
 }
--- src/main/java/com/learning/simplified/services/ContactoService.java
+++ src/main/java/com/learning/simplified/services/ContactoService.java
@@ -16,66 +16,15 @@ import org.springframework.stereotype.Service;
  * @author laura
  */
 @Service
-public class ContactoService {
-    
-    /*
-    public static void crearMensaje(String [] args) {    
-      // Recipient's email ID needs to be mentioned.
-      String to = "simplifiedlearning46@gmail.com";
-
-      // Sender's email ID needs to be mentioned
-      String from = "simplifiedlearning46@gmail.com";
-
-      // Assuming you are sending email from localhost
-      String host = "localhost";
-
-      // Get system properties
-      Properties properties = System.getProperties();
-
-      // Setup mail server
-      properties.setProperty("mail.smtp.host", host);
-
-      // Get the default Session object.
-      Session session = Session.getDefaultInstance(properties);
-
-      try {
-         // Create a default MimeMessage object.
-         MimeMessage message = new MimeMessage(session);
-
-         // Set From: header field of the header.
-         message.setFrom(new InternetAddress(from));
-
-         // Set To: header field of the header.
-         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-         // Set Subject: header field
-         message.setSubject("This is the Subject Line!");
-
-         // Now set the actual message
-         message.setText("This is actual message");
-
-         // Send message
-         Transport.send(message);
-         System.out.println("Sent message successfully....");
-      } catch (MessagingException mex) {
-         mex.printStackTrace();
-      }
-   }
-    
-    
-    
-    
-
-   */
-    
-    @Autowired
+public class ContactoService { 
+@Autowired
 private JavaMailSender mailSender;
 
 public void crearMensaje(String from, String name, String email, String mje, Long phoneNumber) {
     SimpleMailMessage message = new SimpleMailMessage();
   
-    message.setFrom(from);
-    message.setTo("simplifiedlearning@gmail.com"); // Aquí debes especificar la dirección de correo del destinatario
+    message.setFrom("learningsimplified006@gmail.com");
+    message.setTo("learningsimplified006@gmail.com"); // Aquí debes especificar la dirección de correo del destinatario
     message.setSubject("Contacto: " + name);
     message.setText("E-mail: " + email + "\nTeléfono: " + phoneNumber + "\nMensaje: " + mje);
     mailSender.send(message);
